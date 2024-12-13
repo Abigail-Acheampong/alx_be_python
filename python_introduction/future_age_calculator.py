eb50a5394b3a66ee203aedfbf1f6c51d8118bca6
Abigail Owusu Acheampong <owusuacheamponga@gmail.com>
2024-12-13 14:35:02 +0000

    future_age_calculation and hour_to_second file

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+age = int(input("How old are you?   "))
+current_year = 2023
+future_year = 2050
+difference = future_year - current_year
+print(f"In 2050, you will be {age + difference} years old.3-")
