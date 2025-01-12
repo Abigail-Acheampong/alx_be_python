7c04f6d7429221a4312f521c99db717d64d49c39
Abigail Owusu Acheampong <owusuacheamponga@gmail.com>
2025-01-12 07:52:19 +0000

    Returned both the current date and the formatted date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,7 +4,7 @@ def display_current_datetime():
     current_date = datetime.now()
     formatted_datetime = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"The current date and time is {formatted_datetime}.")
-    return current_date
+    return formatted_datetime, current_date
     
 
 def calculate_future_date(current_date, number_of_days):
@@ -12,7 +12,7 @@ def calculate_future_date(current_date, number_of_days):
     print(f"The future date is {future_date}.")
 
 def main():
-    current_date = display_current_datetime()
+    _, current_date = display_current_datetime()
     number_of_days = int(input("Enter the number of days to add to the current date:  "))
     calculate_future_date(current_date, number_of_days)
 
