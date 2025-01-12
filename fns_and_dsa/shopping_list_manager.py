45b9f17935792dc12e553e4e87c60be4073a8140
Abigail Owusu Acheampong <owusuacheamponga@gmail.com>
2025-01-12 04:47:53 +0000

    Added a shopping list manager file.

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,40 @@
+def display_menu():
+    print("\nShopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    """Main function to manage the shopping list"""
+    shopping_list= []
+
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ").strip()
+
+        if choice == '1':
+            #Add on item to the shopping list
+            item = input("Enter the item to add: ").strip()
+            shopping_list.append(item)
+            print(f"{item} is added to the shopping list.")
+        
+        elif choice == '2':
+            #Remove item from the shopping list
+            item = input("Enter the item to add: ").strip()
+            shopping_list.remove(item)
+            print(f"{item} is removed from the shopping list.")
+
+        elif choice == '3':
+            print("The content of the shopping list") 
+            print(shopping_list)
+
+        elif choice == '4':
+            print("Goodbye") 
+            break
+        
+        else:
+            print("Invalid choice. Please try again")
+
+if __name__=="__main__":
+    main()
