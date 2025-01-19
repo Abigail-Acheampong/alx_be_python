0cdd63cb92266c565047befbb8a8e7ba62875338
Abigail Owusu Acheampong <owusuacheamponga@gmail.com>
2025-01-19 18:53:35 +0000

    Corrected version

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,17 +5,22 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.account_balance += amount 
+            print(f"Deposited: ${amount:.2f}")
         else:
             print("Amount should be positive")
     def withdraw(self, amount):
         if amount > 0:
             if amount <= self.account_balance:
                 self.account_balance -= amount
+                print(f"Withdrew: ${amount:.2f}")
                 return True
             else:
+                print("Insufficient funds.")
                 return False
         else:
             print("Withdraw amount must be positive.")
+            return False
+        
     def display_balance(self):
         print("Display the current balance")
         print(f"Your current balance is {self.account_balance:.2f}")
