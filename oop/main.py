ded35e097339cfa8e8b942eab87f64395b7b9100
Abigail Owusu Acheampong <owusuacheamponga@gmail.com>
2025-01-26 13:45:41 +0000

    An example done on polymorphism and overwriting of methods

--- oop/main.py
+++ oop/main.py
@@ -9,20 +9,16 @@
 # if __name__ == "__main__":
 #     main()
 
-from library_system import Book, EBook, PrintBook, Library
+# 
+
+from polymorphism_demo import Shape, Rectangle, Circle
+import math
+
 def main():
-    my_library= Library()
-    classic_book = Book("Pride and Prejudice", "Jane Austen")
-    digital_novel = EBook("Snow Crash", "Neal Stephenson", 500)
-    paper_novel = PrintBook("The Catcher in the Rye", "J.D. Salinger", 234)
+    shapes = [Rectangle(10, 5), Circle(7)]
 
-    # Add books to the library
-    my_library.add_book(classic_book)
-    my_library.add_book(digital_novel)
-    my_library.add_book(paper_novel)
-
-    # List all books in the library
-    my_library.list_books()
+    for shape in shapes:
+        print(f"The area of the {shape.__class__.__name__} is {shape.area()}")
     
 if __name__ == "__main__":
     main()
