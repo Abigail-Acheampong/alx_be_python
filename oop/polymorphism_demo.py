a66147b2738398cbbb5282d494ff5d56be59c00b
Abigail Owusu Acheampong <owusuacheamponga@gmail.com>
2025-01-26 13:59:17 +0000

    created a space between the methods

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -4,16 +4,17 @@ class Shape:
     def area(self):
         raise NotImplementedError("Subclasses must override the area method")
     
-class Circle(Shape):
-    def __init__(self, radius): 
-        self.radius = radius
-    def  area(self):
-        return math.pi*(self.radius**2)
-    
 class Rectangle(Shape):
-    def __init__(self, length, width):
+    def __init__(self, length: float, width: float):
         self.length = length
         self.width = width 
+
     def  area(self):
         return self.length * self.width
     
+class Circle(Shape):
+    def __init__(self, radius: float): 
+        self.radius = radius
+
+    def  area(self):
+        return math.pi * (self.radius**2)
